be90657f75e11e8d3521630f71d8f8922c7359b6
rednz <redentr@hotmail.com>
2019-04-18 14:29:46 +0200

    Menu in progress

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -5,35 +5,39 @@ import android.app.FragmentTransaction;
 
 import android.arch.lifecycle.ReportFragment;
 import android.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
-    public void ChangeFragment(View view) {
-        Fragment fragment;
-
-        if(view == findViewById(R.id.button1)) {
-            fragment = new FrenchFragment1();
-            FragmentManager fm = getFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.fragment_place1, fragment);
-            ft.commit();
-        }
-
-        if(view == findViewById(R.id.button2)) {
-            fragment = new ItalianFragment2();
-            FragmentManager fm = getFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.fragment_place1, fragment);
-            ft.commit();
+    @Override
+    //When pressing back from phone navigation, it closes the menu
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
