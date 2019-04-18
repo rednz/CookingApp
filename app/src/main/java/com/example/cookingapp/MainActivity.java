33c3d4d3116cee6afcb3590ab729c6a2e7298927
rednz <redentr@hotmail.com>
2019-04-18 16:07:29 +0200

    FRAGMENTS R WORKING LADS

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.cookingapp;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-
-import android.arch.lifecycle.ReportFragment;
-import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
+
     private DrawerLayout drawer;
 
     @Override
@@ -25,19 +27,54 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()) {
+
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+                break;
+
+            case R.id.nav_sub_italian1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ItalianPizzaFragment()).commit();
+
+                break;
+
+
+        }
+
+       drawer.closeDrawer(GravityCompat.START);
+
+        return true;//item is selected
     }
 
     @Override
-    //When pressing back from phone navigation, it closes the menu
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+            drawer.closeDrawer(GravityCompat.START); //since its on left side of screen
         } else {
             super.onBackPressed();
         }
     }
+
+
 }
