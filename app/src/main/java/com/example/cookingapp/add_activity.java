7660a6023ca5fe28750cffeddd0c57a0996f716a
rednz <redentr@hotmail.com>
2019-04-22 14:25:14 +0200

    added toast msgs when selecting menu item, menu items are now highlighted when pressed on

--- app/src/main/java/com/example/cookingapp/add_activity.java
+++ app/src/main/java/com/example/cookingapp/add_activity.java
@@ -1,13 +1,35 @@
 package com.example.cookingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class add_activity extends AppCompatActivity {
+
+    private int PICK_IMAGE_REQUEST = 1;
+    private Button btn_choose ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        btn_choose = findViewById(R.id.btn_choosedesign);
+
+        btn_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
 }
