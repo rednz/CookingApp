7d1fd52f8ca4956975e08d9e2d5195cae421b5fc
Jesmar <jesmar_percimes@hotmail.com>
2019-05-02 13:02:36 +0200

    New Activity

--- app/src/main/java/com/example/cookingapp/add_activity.java
+++ app/src/main/java/com/example/cookingapp/add_activity.java
@@ -6,6 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class add_activity extends AppCompatActivity {
@@ -14,7 +29,9 @@ public class add_activity extends AppCompatActivity {
 
     private Button btn_choose ;
 
-
+    private Button btn_submiter;
+    private EditText name;
+    private EditText description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,17 @@ public class add_activity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
 
         btn_choose = findViewById(R.id.btn_choosedesign);
+        name = findViewById(R.id.title);
+        description = findViewById(R.id.recipedetails);
+
+        btn_submiter = findViewById(R.id.btn_submit);
+
+        btn_submiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Submit();
+            }
+        });
 
         btn_choose.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,4 +66,53 @@ public class add_activity extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
 
-}
+    private void Submit() {
+        final String name_recipe = this.name.getText().toString().trim();
+        final String name_details = this.description.getText().toString().trim();
+
+
+        String URL_LOC = "http://10.68.117.144:81/CookingApp/insert_recipe.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOC,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if (success.equals("1")) {
+                                Toast.makeText(add_activity.this, "Recipe uploaded", Toast.LENGTH_SHORT).show();
+
+                                startActivity(new Intent(add_activity.this, MainActivity.class));
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(add_activity.this, "Failed", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(add_activity.this, "Please try again....", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("food_type", getIntent().getStringExtra("food_type").trim());
+                params.put("name", name_recipe);
+                params.put("details", name_details );
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+    }
