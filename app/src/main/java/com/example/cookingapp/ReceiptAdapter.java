742411350e49fd56efe778439ae0c00be2cda415
mirbuh <mirianabuhagiar@live.co.uk>
2019-05-11 16:39:47 +0200

    Floating action button

---
+++ app/src/main/java/com/example/cookingapp/ReceiptAdapter.java
@@ -0,0 +1,70 @@
+package com.example.cookingapp;
+
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+
+
+public class ReceiptAdapter extends RecyclerView.Adapter<ReceiptAdapter.ProductViewHolder> {
+
+
+
+    private Context mCtx;
+    private List<receipt> productList;
+
+    public ReceiptAdapter(Context mCtx, List<receipt> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.generic_layout, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        receipt receipt = productList.get(position);
+
+        //loading the image
+        Glide.with(mCtx)
+                .load(receipt.getImage())
+                .into(holder.imageView);
+
+        holder.textViewTitle.setText(receipt.getTitle());
+        holder.textViewDescription.setText(receipt.getdescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewTitle, textViewDescription;
+        ImageView imageView;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+            imageView = itemView.findViewById(R.id.imageView);
+
+        }
+    }
+}
